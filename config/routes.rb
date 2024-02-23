2a2f3ff4dfa899483981ee91dfde8f7f6fa32303
Sean Donnelly <sean_donnelly@hotmail.co.uk>
2024-02-23 15:54:03 +0000

    Set the root

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: "lists#index"
   resources :lists, only: %i[index new create show destroy] do
     resources :bookmarks, only: %i[new create]
   end
