add8fea6f4b17405be01a3efb73cd374690689bb
Sean Donnelly <sean_donnelly@hotmail.co.uk>
2024-02-23 16:37:54 +0000

    Added validation messages to lists form

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -16,7 +16,11 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     @list.save
-    redirect_to lists_path(@list)
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   # def edit
